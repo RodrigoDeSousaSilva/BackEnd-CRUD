c496c006344009bb71d3f8d1a9e18cde05ea2ae2
rodri <rodrigosilva_s@outlook.com>
2024-12-03 12:37:50 -0300

    return errors fixed

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,19 +1,20 @@
 import { createUser } from "../repository";
-import {validateUserData} from "../validate"
+import { validateUserData } from "../validate";
+import * as yup from "yup";
 
 export const createUserData = async (req, res) => {
   try {
     const params = req.body;
-    const validateUser = await validateUserData.isValid(await params)
-    if (validateUser) {
-      console.log("entrou");
-      const data = await createUser(params);
-      console.log(data);
-      res.status(200).send(data);
-    } else {
-      res.status(400).send(error.errors);
-    }
+    await validateUserData.validate(params, { abortEarly: false })
+
+    const data = await createUser(params)
+    res.status(200).send(data)
+
   } catch (error) {
-    res.status(500).send("Ocorre um erro interno");
+    if (error instanceof yup.ValidationError) {
+      res.status(400).send(error.errors);
+    } else {
+      res.status(500).send("Ocorre um erro interno");
+    }
   }
 };
