e1f92e8fb6755013f269dbd68971a886f272312f
rodri <rodrigosilva_s@outlook.com>
2024-12-07 12:38:55 -0300

    add route to delete user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
-import { createUserData, authenticateUser, updateUserData} from "../controller"
+import { createUserData, authenticateUser, updateUserData, deleteUserData} from "../controller"
 
 const userRoutes = app =>{
     app.post('/createUser', createUserData)
     app.post('/loginUser', authenticateUser)
     app.put('/updateUser', updateUserData)
+    app.delete('/deleteUser', deleteUserData)
 }
 
 export{
